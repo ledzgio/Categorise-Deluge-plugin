85177f3b401217f622ed5feab0cf8a4f982a2a4d
ledzgio <ledzgio@writeme.com>
2010-03-13 18:26:40 +0100

    improved notification by delegating the pyxmpp send_message to a thread, in order to avoid that deluge become slow

--- categorise/send_message.py
+++ categorise/send_message.py
@@ -1,35 +1,48 @@
-'''
-Created on 8/mar/2010
-
-pyxmpp module is required in order to work
-With Debian/ubuntu just type:
-
-sudo apt-get install python-pyxmpp
-'''
 import sys
+import threading
 
 try:
-    pyxmpp_exception = True
+    pyxmpp_exception = False
     from pyxmpp.jid import JID
     from pyxmpp.jabber.simple import send_message
 except ImportError:
     print("pyxmpp module is required to work properly, notificazion won't be sent")
-    pyxmpp_exception = False
+    pyxmpp_exception = True
 
-"""send jabber message using pyxmpp module"""
-def send_msg(message, jabber_id, jabber_password, jabber_recpt):
-    if pyxmpp_exception:
+class PyXmpp:
+    def __init__(self):
+        self.lock = threading.Lock()
+        
+    def send(self, message, jabber_id, jabber_password, jabber_recpt):
         try:
-            jid = jabber_id
-            password = jabber_password
-            recpt = jabber_recpt
+            self.lock.acquire()#acquire lock
             subject = "Deluge Torrent completed"
-            body=message
-            jid=JID(jid)
+            jid=JID(jabber_id)
             if not jid.resource:
                 jid=JID(jid.node,jid.domain,"send_message")
-                recpt=JID(recpt)
-                send_message(jid,password,recpt,body,subject)
-        except Error:
-            pass
-    return pyxmpp_exception
+                recpt=JID(jabber_recpt)
+                send_message(jid, jabber_password, recpt, message, subject)
+        finally:
+            self.lock.release();#release lock
+
+"""send jabber message using pyxmpp module (starting new thread)"""
+class PyXmppThread(threading.Thread):
+    def __init__(self, message, jabber_id, jabber_password, jabber_recpt):
+        threading.Thread.__init__(self)
+        self.message=message
+        self.jabber_id=jabber_id
+        self.jabber_password=jabber_password
+        self.jabber_recpt=jabber_recpt
+        
+    def run(self):
+        pyxmpp = PyXmpp()
+        pyxmpp.send(self.message, self.jabber_id, self.jabber_password, self.jabber_recpt)
+    
+
+def send_msg(message, jabber_id, jabber_password, jabber_recpt):
+    if not pyxmpp_exception:
+        xmpp = PyXmppThread(message, jabber_id, jabber_password, jabber_recpt)#send message
+        xmpp.start()
+        return True
+    else:
+        return False
