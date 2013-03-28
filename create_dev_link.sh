115a8f803cf37129de749a2d87a1179fc3b179f7
ledzgio <ledzgio@writeme.com>
2013-03-28 21:55:06 +0100

    remove build script

--- create_dev_link.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/giorgio/git/Categorise-Deluge-plugin
-mkdir temp
-export PYTHONPATH=./temp
-python setup.py build develop --install-dir ./temp
-cp ./temp/Categorise.egg-link /home/giorgio/.config/deluge/plugins
-rm -fr ./temp
